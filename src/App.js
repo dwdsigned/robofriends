a9127bf232ed4adb804603fd8bc31e5a38cec4f3
Darius Williams <dariustwilliams668@gmail.com>
2023-12-19 17:01:11 -0500

    my first react component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -14,12 +16,13 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+          >
+          Learning React
         </a>
-      </header>
+        </header>
     </div>
   );
+  }
 }
 
 export default App;
