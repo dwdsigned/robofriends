8f02cafe74e88287100932a94262678980b9273a
Darius Williams <dariustwilliams668@gmail.com>
2024-01-01 15:54:06 -0500

    review

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import'tachyons';
 import { robots } from './robots'
 
+// Reviewed previous react
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
