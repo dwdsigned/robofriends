2177eb1da5e302fd92bccffd563c92c5f3f6b78f
Darius Williams <dariustwilliams668@gmail.com>
2024-01-01 15:55:28 -0500

    removed review comment :)

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reportWebVitals from './reportWebVitals';
 import'tachyons';
 import { robots } from './robots'
 
-// Reviewed previous react
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
